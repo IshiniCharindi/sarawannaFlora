9e2fb87c097739b956682462623a43384fbbaa5d
sachinduKavi <cst21017@std.uwu.ac.lk>
2025-04-08 21:35:39 +0530

    admin panel

--- src/features/admin/AdminBase.tsx
+++ src/features/admin/AdminBase.tsx
@@ -3,14 +3,22 @@ import { Button } from 'antd'
 import { useDispatch } from 'react-redux'
 import { setLoading } from '../../redux/loading-slice'
 import { AppDispatch } from '../../redux/store'
+import ControlPanel from './ControlPanel'
 
 export default function AdminBase() {
   const dispatch: AppDispatch = useDispatch()
 
   return (
-    <div className='screen'>
+    <div className='screen flex-row !justify-start'>
+        <ControlPanel/>
         
-        
+        <div className="content flex-1 flex-column">
+            <div className="top-bar w-full p-4 bg-red-500 flex-row justify-between items-center">
+              <div>Hello</div>
+              <div>Hello</div>
+
+            </div>
+        </div>
     </div>
   )
 }
---
+++ src/features/admin/ControlPanel.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ControlPanel() {
+  return (
+    <div className='h-screen w-[300px] bg-[#101A33] drop-shadow-lg flex flex-col justify-between items-center'>
+        <div className='flex-column justify-center items-center h-full'>
+        <h1 className="heading text-white pt-4 font-bold text-3xl ">Sarvanna Floral</h1>
+        <h3 className='text-white font-bold text-center'>CONTROL PANEL</h3>
+        </div>
+    </div>
+  )
+}
