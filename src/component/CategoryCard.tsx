29eb9d20eae0b4d11edb81cc45425b96c56ab084
Sajan-Hirusha <141469578+Sajan-Hirusha@users.noreply.github.com>
2025-04-09 03:29:25 +0530

    fixing category slider css issues

--- src/component/CategoryCard.tsx
+++ src/component/CategoryCard.tsx
@@ -10,7 +10,7 @@ function CategoryCard(props: any) {
                 className={`rounded-t-lg w-full h-[250px] object-cover transition-transform duration-1000 ease-in-out transform hover:scale-110`}
                 style={{ objectFit: 'cover' }}
             />
-            <div className="!border-gray-300 p-4">
+            <div className="bg-gray-100 p-4">
                 <p className="text-gray-500 text-xs mb-2">{props.category}</p>
                 <h3 className="text-sm font-semibold text-gray-800 mb-4 truncate">
                     {props.itemName}
