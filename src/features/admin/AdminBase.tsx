9d4db5181eb99b27253fdc05902306f96d5bc14c
sachinduKavi <cst21017@std.uwu.ac.lk>
2025-04-08 23:11:42 +0530

    admin panel

--- src/features/admin/AdminBase.tsx
+++ src/features/admin/AdminBase.tsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Button } from 'antd'
 import { useDispatch } from 'react-redux'
 import { setLoading } from '../../redux/loading-slice'
 import { AppDispatch } from '../../redux/store'
 import ControlPanel from './ControlPanel'
+import menuIcon from '../../assets/icons/menu-icon.svg'
+import {motion} from 'framer-motion'
+
+
 
 export default function AdminBase() {
+  const [panelOpen, setPanelOpen] = useState<boolean>(true)
   const dispatch: AppDispatch = useDispatch()
 
   return (
     <div className='screen flex-row !justify-start'>
-        <ControlPanel/>
+        {panelOpen && <ControlPanel/>}
         
         <div className="content flex-1 flex-column">
-            <div className="top-bar w-full p-4 bg-red-500 flex-row justify-between items-center">
-              <div>Hello</div>
-              <div>Hello</div>
+            <div className="top-bar w-full p-4 flex-row justify-between items-center">
+              <motion.button
+                whileTap={{ scale: 0.9 }}
+                onClick={() => setPanelOpen(pre => !pre)}
+                className='border-none cursor-pointer w-[35px] p-0.5 focus:drop-shadow-2xl'
+              >
+                <img src={menuIcon} alt="" />
+              </motion.button>
 
             </div>
         </div>
--- src/features/admin/ControlPanel.tsx
+++ src/features/admin/ControlPanel.tsx
@@ -1,12 +1,36 @@
 import React from 'react'
+import {motion} from 'framer-motion'
+import companyLogo from '../../assets/logo/company-logo.png'
 
 export default function ControlPanel() {
   return (
-    <div className='h-screen w-[300px] bg-[#101A33] drop-shadow-lg flex flex-col justify-between items-center'>
-        <div className='flex-column justify-center items-center h-full'>
+    <motion.div 
+    initial={{x: -300}}
+    animate={{x: 0}}
+    exit={{x: -300}}
+    transition={{duration: 0.2}}
+    className='h-screen w-[300px] bg-[#101A33] drop-shadow-lg flex flex-col justify-between items-center'>
+        <div className='flex-column justify-center items-center'>
         <h1 className="heading text-white pt-4 font-bold text-3xl ">Sarvanna Floral</h1>
         <h3 className='text-white font-bold text-center'>CONTROL PANEL</h3>
         </div>
-    </div>
+
+
+        <div className="content flex flex-col w-full flex-grow justify-center items-center">
+            <div className="inner-content m-5 flex flex-col justify-center items-center w-full">
+                <button className='text-white font-bold m-2.5'>New Product</button>
+                <button className='text-white font-bold m-2.5'>Manage Products</button>
+                <button className='text-white font-bold m-2.5'>Orders</button>
+                <button className='text-white font-bold m-2.5'>Settings</button>
+                <button className='text-white font-bold m-2.5'>Logout</button>
+            </div>
+        </div>
+
+        <div className="company flex w-full flex-col aspect-square justify-center items-center p-5 rounded-lg border-white">
+            {/* <h2 className='text-xl text-white font-bold text-center'>We embroid your dreams</h2> */}
+            <img src={companyLogo} alt="Soft detroits logo" className='w-[80%]'/>
+            <h4 className='text-white'>Hotline: +94 76 431 4505</h4>
+        </div>
+    </motion.div>
   )
 }
