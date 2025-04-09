7bd724dfc4d7d5120672d46a4fa7d1c1be9f988e
Sajan-Hirusha <141469578+Sajan-Hirusha@users.noreply.github.com>
2025-04-10 02:36:28 +0530

    Changing css

--- src/component/Shop/ProductDetails.tsx
+++ src/component/Shop/ProductDetails.tsx
@@ -97,7 +97,7 @@ const ProductDetails = () => {
                         </div>
 
                         <button
-                            className="bg-green-600 text-white h-12 px-6 rounded-lg hover:bg-green-700 transition-colors flex items-center justify-center gap-2 w-full">
+                            className="bg-yellow-600 text-white h-12 px-6 rounded-lg hover:bg-yellow-500 transition-colors flex items-center justify-center gap-2 w-full">
                             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
                                       d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
