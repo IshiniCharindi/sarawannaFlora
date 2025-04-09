3842d086b2173928e4f2fcdbe231394f99f7a633
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2025-04-10 00:12:18 +0530

    contact details component

--- src/features/admin/ContactUs.tsx
+++ src/features/admin/ContactUs.tsx
@@ -3,6 +3,7 @@ import NavBar from "../../component/Other/NavBar.tsx";
 import contactUsBanner from "../../assets/images/contactUsBanner.jpg";
 import TopBanner from "../../component/Other/TopBanner.tsx";
 import ContactUsMap from "../../component/ContactUs/ContactUsMap.tsx";
+import ContactDetails from "../../component/ContactUs/ContactDetails.tsx";
 
 const ContactUs = () => {
     return (
@@ -13,6 +14,7 @@ const ContactUs = () => {
                 heading={"Your Gateway to International Markets"}
                 para={"Expand globally with our reliable export services."}/>
             <ContactUsMap/>
+            <ContactDetails/>
         </div>
     );
 };
