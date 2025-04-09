4182ba3d9e2ad70ed5efe5f81c5df9e69be662e6
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2025-04-10 00:36:00 +0530

    added name attirbutes

--- src/component/ContactUs/ContactForm.tsx
+++ src/component/ContactUs/ContactForm.tsx
@@ -13,40 +13,40 @@ const ContactForm = () => {
                                         <div>
                                             <label htmlFor="" className="text-base font-medium text-gray-900"> Your name </label>
                                             <div className="mt-2.5 relative">
-                                                <input type="text" name="" id="" placeholder="Enter your full name" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
+                                                <input type="text" name="name" id="" placeholder="Enter your full name" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
                                             </div>
                                         </div>
 
                                         <div>
                                             <label htmlFor="" className="text-base font-medium text-gray-900"> Email address </label>
                                             <div className="mt-2.5 relative">
-                                                <input type="email" name="" id="" placeholder="Enter your full name" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
+                                                <input type="email" name="email" id="" placeholder="Enter your full name" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
                                             </div>
                                         </div>
 
                                         <div>
                                             <label htmlFor="" className="text-base font-medium text-gray-900"> Phone number </label>
                                             <div className="mt-2.5 relative">
-                                                <input type="tel" name="" id="" placeholder="Enter your full name" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
+                                                <input type="tel" name="phoneNumber" id="" placeholder="Enter your full name" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
                                             </div>
                                         </div>
 
                                         <div>
                                             <label htmlFor="" className="text-base font-medium text-gray-900"> Company name </label>
                                             <div className="mt-2.5 relative">
-                                                <input type="text" name="" id="" placeholder="Enter your full name" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
+                                                <input type="text" name="CompanyName" id="" placeholder="Enter your full name" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
                                             </div>
                                         </div>
 
                                         <div className="sm:col-span-2">
                                             <label htmlFor="" className="text-base font-medium text-gray-900"> Message </label>
                                             <div className="mt-2.5 relative">
-                                                <textarea name="" id="" placeholder="" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md resize-y focus:outline-none focus:border-blue-600 caret-blue-600" rows="4"></textarea>
+                                                <textarea name="message" id="" placeholder="" className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md resize-y focus:outline-none focus:border-blue-600 caret-blue-600" rows="4"></textarea>
                                             </div>
                                         </div>
 
-                                        <div class="sm:col-span-2">
-                                            <button type="submit" class="inline-flex items-center justify-center w-full px-4 py-4 mt-2 text-base font-semibold text-white transition-all duration-200 bg-[var(--color-primary)] border border-transparent rounded-md focus:outline-none hover:bg-[var(--color-light)] focus:bg-[var(--color-light)]">
+                                        <div className="sm:col-span-2">
+                                            <button type="submit" className="inline-flex items-center justify-center w-full px-4 py-4 mt-2 text-base font-semibold text-white transition-all duration-200 bg-[var(--color-primary)] border border-transparent rounded-md focus:outline-none hover:bg-[var(--color-light)] focus:bg-[var(--color-light)]">
                                                 Send
                                             </button>
                                         </div>
